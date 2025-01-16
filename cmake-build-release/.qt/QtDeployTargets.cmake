a8dcfd1a57f1aafb233f2eae2732c1d5f0956951
decmofofs <decmofofs@gmail.com>
2025-01-16 22:44:59 +0800

    Implemented element path

--- cmake-build-release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SVGForge_FILE D:/WorkSpace/SVGForge/cmake-build-release/SVGForge.exe)
-set(__QT_DEPLOY_TARGET_SVGForge_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SVGForge_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Svg.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
